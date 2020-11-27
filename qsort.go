5eebf5bb39b708166d9ed7cc5ba7837723f1c58d
Rob <rob.rimmer@mclaren.com>
2020-11-27 18:34:50 +0000

    goroutines

--- qsort.go
+++ qsort.go
@@ -1,25 +1,33 @@
 package main
 
+import "sync"
+
 func Qsort(list []int) {
-	qsort(list, 0, len(list)-1)
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go qsort(list, 0, len(list)-1, &wg)
+	wg.Wait()
 }
 
-func qsort(list []int, start int, end int) {
-	if end <= start {
-		return
-	}
-	pivot := list[end]
-	j := start - 1
-	for i := start; i < end; i++ {
-		if list[i] < pivot {
-			j++
-			swap(list, i, j)
+func qsort(list []int, start int, end int, wg *sync.WaitGroup) {
+	if end > start {
+		pivot := list[end]
+		j := start - 1
+		for i := start; i < end; i++ {
+			if list[i] < pivot {
+				j++
+				swap(list, i, j)
+			}
 		}
-	}
-	swap(list, j+1, end)
+		swap(list, j+1, end)
 
-	qsort(list, start, j)
-	qsort(list, j+2, end)
+		wg2 := sync.WaitGroup{}
+		wg2.Add(2)
+		go qsort(list, start, j, &wg2)
+		go qsort(list, j+2, end, &wg2)
+		wg2.Wait()
+	}
+	wg.Done()
 }
 
 func swap(list []int, idx1 int, idx2 int) {
